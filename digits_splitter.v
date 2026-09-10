4d1a571fce54c327220192c5de7a3e8900c6f201
SriCharumathy <charushree2122@gmail.com>
2026-09-10 06:07:31 +0000

    Updata

--- digits_splitter.v
+++
@@ -1,7 +0,0 @@
-module digital_splitter(input [5:0] sec,min,output [3:0] sec_tens,sec_ones,min_tens,min_ones);
-assign sec_tens = sec / 10;
-assign sec_ones = sec % 10;
-
-assign min_tens = min / 10;
-assign min_ones = min % 10;
-endmodule
